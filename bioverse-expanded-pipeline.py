098d572f36c48fc8628a072be109ee33ef7a647e
Tessa Fisher <tfisher4@asu.edu>
2024-01-31 13:53:18 -0700

    Working on CH4

--- bioverse-expanded-pipeline.py
+++ bioverse-expanded-pipeline.py
@@ -9,27 +9,27 @@ from tqdm import tqdm, trange
 import numpy as np
 import matplotlib.pyplot as plt
 
-atmos = pyatmos.Simulation(code_path='~/atmos-master', DEBUG=True)
+atmos = pyatmos.Simulation(code_path='/media/tessa/Storage/Dissertation_backup/atmos-master',docker_image=None, DEBUG=True)
 atmos.start()
 
 curr_dir=os.getcwd()
 
-O2_flux_list=[5E+10,9E+10,3.94985E+11,8E+11]
+CH4_flux_list=[1E-2,1E-1,1,1E2,1E4,1E6,1E8,1E10,1E12,1E13]
 
 
-for i in range(len(O2_flux_list)):
-    O2_flux=O2_flux_list[i]
-    output_dir=curr_dir+'/sample_atmos_results/O2_test_{n}'.format(n=i)
-    args = {
-        'species_fluxes': {'O2' :O2_flux},
-        'max_photochem_iterations' : 50000, 
-        'max_clima_steps' : 10, 
-        'output_directory' : output_dir}
+# for i in range(len(CH4_flux_list)):
+    # CH4_flux=CH4_flux_list[i]
+    # output_dir=curr_dir+'/sample_atmos_results/CH4_test_{n}'.format(n=i)
+    # args = {
+        # 'species_fluxes': {'CH4' :CH4_flux},
+        # 'max_photochem_iterations' : 50000, 
+        # 'max_clima_steps' : 10, 
+        # 'output_directory' : output_dir}
     
     
-    atmos.run(**args)
+    # atmos.run(**args)
 
-model_list=glob.glob('sample_atmos_results/O2_test*')
+model_list=glob.glob('sample_atmos_results/CH4_test*')
 
 tel='JWST'
 
@@ -41,7 +41,6 @@ survey=TransitSurvey('default')
 sample, detected, data = survey.quickrun(generator, t_total=10*365.25)
 T_st_ref = survey.T_st_ref
 R_st_ref = survey.R_st_ref
-survey.measurements['has_H2O'].t_ref=7.5
 
 
 
@@ -52,22 +51,22 @@ for i in range(len(model_list)):
 
     #create the null spectrum for t_ref
     
-    null_newf=atmosatm(model_list[i],tel=tel,filebase=model,null_spec=True,removed_gas="O2",star='M')
+    null_newf=atmosatm(model_list[i],tel=tel,filebase=model,null_spec=True,removed_gas="CH4",star='G')
     psgspec(model,null_newf,showplot=False,null_spec=True)
     null_rad=curr_dir+'/psg_output/%s_null_rad.txt' % model
 
     #create radiance spectrum
 
-    newf=atmosatm(model_list[i],tel=tel,filebase=model,star='M')
+    newf=atmosatm(model_list[i],tel=tel,filebase=model,star='G')
     psgspec(model,newf,showplot=False)
     model_rad=curr_dir+'/psg_output/%s_rad.txt' % model
     t_ref = compute_t_ref(filenames=(model_rad,null_rad), t_exp=10, wl_min=0.4, wl_max=0.9,)
     print("Required exposure time for %s: {:.1f} hr".format(t_ref) % model)
-    survey.measurements['has_O2'].t_ref=t_ref/24
+    survey.measurements['has_CH4'].t_ref=t_ref/24
     survey.save()
     
     
-    t_exp, N_obs = survey.measurements['has_O2'].compute_exposure_time(data[detected['EEC']])
+    t_exp, N_obs = survey.measurements['has_CH4'].compute_exposure_time(data[detected['EEC']])
 
     fig, ax = plt.subplots(ncols=2, figsize=(16,8))
 
